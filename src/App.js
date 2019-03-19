b87753cbe730e938be389e064550a8fefdba62ea
张威 <zw900808@gmail.com>
2019-03-19 14:14:04 +0800

    解决发布到github 的gh-pages分支出现history报错，使用HashRouter就没问题

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch } from "react-router";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { renderRoutes } from "react-router-config";
 import { ConnectedRouter } from "connected-react-router";
@@ -17,10 +17,9 @@ const App = () => (
       <ConnectedRouter history={history}>
         <>
           {/* your usual react-router v4 routing */}
-          <Switch>
-            {renderRoutes(routes)}
-            <Router component={() => <div>df</div>} />
-          </Switch>
+          <HashRouter>
+            <Switch>{renderRoutes(routes)}</Switch>
+          </HashRouter>
         </>
       </ConnectedRouter>
     </PersistGate>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import LayoutPage from "../components/layout/LayoutPage";
 import Login from "../pages/login/Login";
 import menus from "../config/menu.config";
+import NoMatch from "../pages/NoMatch";
 
 //不同的用户不同的权限显示的路由也是变的
 const routes = [
@@ -13,6 +14,9 @@ const routes = [
     component: LayoutPage,
     routes: menus
   }
+  // {
+  //   component: NoMatch
+  // }
 ];
 
 export default routes;
