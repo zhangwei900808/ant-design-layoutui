b8c8042375f1e6c90296de9cdecc15a30c4fa901
张威 <zw900808@gmail.com>
2019-03-19 10:30:30 +0800

    Updates

---
+++ precache-manifest.02c1f3cd41b7db8f61dcfe8da380025b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d64474a8d23b32f731e9f67ccf01fa57",
+    "url": "/ant-design-layoutui/static/media/right.d64474a8.png"
+  },
+  {
+    "revision": "782ce78cf78a16729e31",
+    "url": "/ant-design-layoutui/static/js/runtime~main.2bbab2d2.js"
+  },
+  {
+    "revision": "344344e36433dec6f9f4",
+    "url": "/ant-design-layoutui/static/js/main.4788e831.chunk.js"
+  },
+  {
+    "revision": "8035e51ffc47eb906bb2",
+    "url": "/ant-design-layoutui/static/js/2.c2978a44.chunk.js"
+  },
+  {
+    "revision": "344344e36433dec6f9f4",
+    "url": "/ant-design-layoutui/static/css/main.0e275ba3.chunk.css"
+  },
+  {
+    "revision": "8035e51ffc47eb906bb2",
+    "url": "/ant-design-layoutui/static/css/2.7cb7dc74.chunk.css"
+  },
+  {
+    "revision": "69a0d223729b0fc4988dec2f1220d686",
+    "url": "/ant-design-layoutui/index.html"
+  }
+];
