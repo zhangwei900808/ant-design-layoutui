007bbb06b9d2bef7278b69e3aed72139f2daea9d
张威 <zw900808@gmail.com>
2019-04-08 11:48:19 +0800

    添加模型

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,7 +1,8 @@
 import { all, fork } from "redux-saga/effects";
 import userSaga from "./userSaga";
+import authSaga from "./authSaga";
 
 /*添加对action的监听 */
 export default function* rootSaga() {
-  yield all([fork(userSaga)]);
+  yield all([fork(userSaga), fork(authSaga)]);
 }
